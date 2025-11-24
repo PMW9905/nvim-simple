cc02ea28100b62823b177a1ae7bab4c18d5dd7fc
Parker <pmw9905@gmail.com>
2025-11-24 15:11:20 -0600

    added simple keymaps for lsp

--- init.lua
+++ init.lua
@@ -7,29 +7,31 @@ vim.o.shiftwidth = 4
 
 vim.o.swapfile = false
 
+vim.o.winborder = 'rounded'
+
 -- leader
 vim.g.mapleader = " "
 
 -- theme
-vim.pack.add({{ src = "https://github.com/sainnhe/everforest" }})
+vim.pack.add({ { src = "https://github.com/sainnhe/everforest" } })
 vim.cmd("colorscheme everforest")
 
 -- transparency
-vim.pack.add({{ src = "https://github.com/xiyaowong/transparent.nvim" }})
+vim.pack.add({ { src = "https://github.com/xiyaowong/transparent.nvim" } })
 
 require("transparent").setup({
-  extra_groups = {
-    "NormalFloat",     -- Floating window background
-    "FloatBorder",     -- Floating window border
-    "FloatTitle",      -- Floating window title (if any)
-  },
+	extra_groups = {
+		"NormalFloat", -- Floating window background
+		"FloatBorder", -- Floating window border
+		"FloatTitle", -- Floating window title (if any)
+	},
 })
 
 vim.g.transparent_enabled = true
 
 -- status bar
-vim.pack.add({{ src = "https://github.com/nvim-lualine/lualine.nvim" }})
-vim.pack.add({{ src = "https://github.com/nvim-tree/nvim-web-devicons" }})
+vim.pack.add({ { src = "https://github.com/nvim-lualine/lualine.nvim" } })
+vim.pack.add({ { src = "https://github.com/nvim-tree/nvim-web-devicons" } })
 require('lualine').setup({
 	options = {
 		theme = 'everforest'
@@ -37,34 +39,34 @@ require('lualine').setup({
 })
 
 -- autopairs
-vim.pack.add({{ src ="https://github.com/windwp/nvim-autopairs" }})
+vim.pack.add({ { src = "https://github.com/windwp/nvim-autopairs" } })
 require('nvim-autopairs').setup({
 	event = "InsertEnter",
 	config = true,
 })
 
 -- terminal
-vim.pack.add({{ src = "https://github.com/akinsho/toggleterm.nvim"}})
+vim.pack.add({ { src = "https://github.com/akinsho/toggleterm.nvim" } })
 require('toggleterm').setup({
 	open_mapping = "<C-k>",
-	direction= 'float',
+	direction = 'float',
 	float_opts = {
 		border = 'curved',
 	}
 })
 
--- file grep 
-vim.pack.add({{ src = "https://github.com/nvim-lua/plenary.nvim"}})
-vim.pack.add({{ src = "https://github.com/nvim-telescope/telescope.nvim"}})
+-- file grep
+vim.pack.add({ { src = "https://github.com/nvim-lua/plenary.nvim" } })
+vim.pack.add({ { src = "https://github.com/nvim-telescope/telescope.nvim" } })
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set({'n','i','v','t'}, '<C-p>', telescope_builtin.find_files)
-vim.keymap.set({'n','i','v','t'}, '<C-l>', telescope_builtin.live_grep)
+vim.keymap.set({ 'n', 'i', 'v', 't' }, '<C-p>', telescope_builtin.find_files)
+vim.keymap.set({ 'n', 'i', 'v', 't' }, '<C-l>', telescope_builtin.live_grep)
 
 -- file explorer
-vim.pack.add({{ src = "https://github.com/mikavilpas/yazi.nvim" }})
+vim.pack.add({ { src = "https://github.com/mikavilpas/yazi.nvim" } })
 require("yazi").setup({
-  yazi_floating_window_winblend = 0,
-  floating_window_scaling_factor = 0.8, -- Default is 0.9, makes window 90% of screen
+	yazi_floating_window_winblend = 0,
+	floating_window_scaling_factor = 0.8,
 })
 vim.keymap.set("n", "<C-y>", function()
 	require("yazi").yazi()
@@ -72,9 +74,9 @@ end)
 
 -- lsp
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig"},
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim"},
-	{ src = "https://github.com/mason-org/mason.nvim"}
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" }
 })
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -82,3 +84,7 @@ require("mason-lspconfig").setup({
 })
 
 vim.lsp.enable('lua-language-server')
+
+vim.keymap.set("n", '<leader>fm', vim.lsp.buf.format)
+vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set("n", '<leader>h', vim.lsp.buf.hover)
