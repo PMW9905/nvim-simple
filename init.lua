682cb1dd69262a42d7c77ef3d88b47f0fe855161
Parker <pmw9905@gmail.com>
2025-11-24 01:06:47 -0600

    init commit

---
+++ init.lua
@@ -0,0 +1,36 @@
+-- basic settings
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.swapfile = false
+
+-- leader
+vim.g.mapleader = " "
+
+-- theme
+vim.pack.add({{ src = "https://github.com/sainnhe/everforest" }})
+vim.cmd("colorscheme everforest")
+
+-- transparency
+vim.pack.add({{ src = "https://github.com/xiyaowong/transparent.nvim" }})
+vim.g.transparent_enabled = true
+
+-- status bar
+vim.pack.add({{ src = "https://github.com/nvim-lualine/lualine.nvim" }})
+vim.pack.add({{ src = "https://github.com/nvim-tree/nvim-web-devicons" }})
+
+require('lualine').setup({
+	options = {
+		theme = 'everforest'
+	}
+})
+
+-- autopairs
+vim.pack.add({{ src ="https://github.com/windwp/nvim-autopairs" }})
+require('nvim-autopairs').setup({
+	event = "InsertEnter",
+	config = true,
+})
