414acf5d118be7c13028fa934898b8ed78b3e62c
Parker Whitehead <pmw9905@gmail.com>
2025-11-25 13:03:00 -0600

    added treesitter

--- init.lua
+++ init.lua
@@ -77,6 +77,16 @@ vim.keymap.set("n", "<C-y>", function()
 	require("yazi").yazi()
 end)
 
+-- syntax highlighting
+vim.pack.add({
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter'}
+})
+require('nvim-treesitter.configs').setup({
+	ensure_installed = {
+		'lua', 'gdscript'
+	},
+	auto_install = true
+})
 
 -- lsp
 vim.pack.add({
