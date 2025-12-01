0dbdde66af128794bd020c6c329bded23b2803e3
Parker Whitehead <pmw9905@gmail.com>
2025-12-01 05:45:41 -0600

    fixed go lsp

--- init.lua
+++ init.lua
@@ -99,7 +99,7 @@ vim.pack.add({
 	{ src = "https://github.com/mason-org/mason.nvim" }
 })
 
-local enabled_language_servers = { 'lua_ls', 'yamlls', 'go'}
+local enabled_language_servers = { 'lua_ls', 'yamlls', 'gopls'}
 
 require("mason").setup()
 require("mason-lspconfig").setup({
